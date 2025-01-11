9f77839fb196eb7c5eb78c13a4f5009a7cc2065a
Ibrahimkhan1903@outlook.com <Ibrahimkhan1903@outlook.com>
2025-01-11 15:40:47 +0500

    Console.WriteLine modified

--- APP_Using_Git/Program.cs
+++ APP_Using_Git/Program.cs
@@ -3,3 +3,5 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Hello My Name is Muhammad Ibrahim");
 Console.WriteLine("I've just started with Git and made the first Repository Successful.");
+Console.WriteLine("After commiting all the changes let's try this one...");
+Console.WriteLine("Really happy to work with...");
