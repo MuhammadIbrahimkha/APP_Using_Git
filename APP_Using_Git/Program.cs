9bf618e0da6d549df30b2cc3cb084322aa70bdc4
Muhammad Ibrahim <165913969+MuhammadIbrahimkha@users.noreply.github.com>
2025-01-11 15:52:41 +0500

    Remove the duplicate Messages

--- APP_Using_Git/Program.cs
+++ APP_Using_Git/Program.cs
@@ -10,10 +10,4 @@ Console.WriteLine("Hmmm! Changes... from Visual studio to Github Repository");
 Console.WriteLine("Changes from Github Repository to Visual Studio");
 Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Hello My Name is Muhammad Ibrahim");
-Console.WriteLine("I've just started with Git and made the first Repository Successful.");
-Console.WriteLine("After commiting all the changes let's try this one...");
-Console.WriteLine("Really happy to work with...");
-Console.WriteLine("One more modification");
-Console.WriteLine("Hmmm! Changes... from Visual studio to Github Repository");
-Console.WriteLine("Changes from Github Repository to Visual Studio");
+
