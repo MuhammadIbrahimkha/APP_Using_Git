63a9170e0f5d92f31955584e3108a43c47a9eab4
Muhammad Ibrahim <165913969+MuhammadIbrahimkha@users.noreply.github.com>
2025-01-11 15:49:28 +0500

    Change from Github Repository to Visual Studio

--- APP_Using_Git/Program.cs
+++ APP_Using_Git/Program.cs
@@ -7,3 +7,4 @@ Console.WriteLine("After commiting all the changes let's try this one...");
 Console.WriteLine("Really happy to work with...");
 Console.WriteLine("One more modification");
 Console.WriteLine("Hmmm! Changes... from Visual studio to Github Repository");
+Console.WriteLine("Changes from Github Repository to Visual Studio");
